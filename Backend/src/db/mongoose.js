a1d4a570f1319896cb9d00ab89263df37c53e138
Yaroslav Olefirenko <Yar14ka@gmail.com>
2021-10-21 17:00:13 +0300

    create user model and user routers

---
+++ Backend/src/db/mongoose.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+
+const url =
+  'mongodb+srv://Yaroslav:qwerty1234@cluster0.p52ub.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+mongoose.connect(url);
